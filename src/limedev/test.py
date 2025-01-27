40c682eda9268d7a33f368161973525a62d1bb88
Vili Vilenius <vili.vilenius@outlook.com>
2025-01-27 10:46:58 +0200

    gitignore update, test to use realtime priority

--- src/limedev/test.py
+++ src/limedev/test.py
@@ -235,6 +235,22 @@ def benchmarking(path_benchmarks: Path = PATH_TESTS / 'benchmarking.py',
                  out: Path | None = None,
                  **kwargs: str) -> int:
     """Runs performance tests and save results into YAML file."""
+    from sys import platform
+    # Setting process to realtime
+    if platform == 'win32':
+        # Based on:
+        #   "Recipe 496767: Set Process Priority In Windows" on ActiveState
+        #   http://code.activestate.com/recipes/496767/
+        import win32api, win32process, win32con
+
+        pid = win32api.GetCurrentProcessId()
+        handle = win32api.OpenProcess(win32con.PROCESS_ALL_ACCESS, True, pid)
+        win32process.SetPriorityClass(handle,
+                                        win32process.REALTIME_PRIORITY_CLASS)
+    elif platform == 'linux':
+        import os
+
+        os.nice(-20 - os.nice()) # type: ignore[attr-defined]
 
     version, results = import_from_path(path_benchmarks).main(**kwargs)
 
