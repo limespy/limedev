8ec261c3c384fcdf7ccff4409e1de3f2670d0e0c
Vili Vilenius <vili.vilenius@outlook.com>
2025-06-18 20:45:23 +0300

    some fixes?

--- src/limedev/__init__.py
+++ src/limedev/__init__.py
@@ -1,8 +1,25 @@
 """Tools for testing, building readme and packaging."""
-from ._API import * # noqa: F403
+from typing import TYPE_CHECKING
+
+from ._api import * # noqa: F403
+
+if TYPE_CHECKING:
+    from types import ModuleType
+    from . import cli as cli
+    from . import package as package
+else:
+    ModuleType = object
 # ======================================================================
-def __getattr__(name: str) -> str:
+def __getattr__(name: str) -> str | ModuleType:
     if name == '__version__':
         from importlib import metadata
         return metadata.version(__package__)
+
+    if name in {'cli', 'package'}:
+        from importlib import import_module
+        from sys import modules as _modules
+
+        module = import_module(f'.{name}', __package__)
+        setattr(_modules[__package__], name, module)
+        return module
     raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
